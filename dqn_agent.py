470406a0e640b26d64ebbd5c211c84fd74c083da
Alex Broekhof <abroekhof@google.com>
2020-05-10 09:20:57 -0700

    Some work on prioritization

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from collections import namedtuple, deque
+import heapq
 
 from model import QNetwork
 
@@ -49,11 +50,12 @@ class Agent():
 
         # Learn every UPDATE_EVERY time steps.
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
+
         if self.t_step == 0:
             # If enough samples are available in memory, get random subset and learn
             if len(self.memory) > BATCH_SIZE:
-                experiences = self.memory.sample()
-                self.learn(experiences, GAMMA)
+
+                self.learn(GAMMA)
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy.
@@ -75,7 +77,7 @@ class Agent():
         else:
             return random.choice(np.arange(self.action_size))
 
-    def learn(self, experiences, gamma):
+    def learn(self, gamma):
         """Update value parameters using given batch of experience tuples.
 
         Params
@@ -83,6 +85,9 @@ class Agent():
             experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples
             gamma (float): discount factor
         """
+
+        indices = self.memory.sample()
+        experiences = self.memory.indices_to_experiences(indices)
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
@@ -92,6 +97,10 @@ class Agent():
 
         # Get expected Q values from local model
         Q_expected = self.qnetwork_local(states).gather(1, actions)
+
+        with torch.no_grad():
+            priorities = np.concatenate(abs(Q_targets - Q_expected).numpy())
+            self.memory.update_priorities(indices, priorities)
 
         # Compute loss
         loss = F.mse_loss(Q_expected, Q_targets)
@@ -131,25 +140,87 @@ class ReplayBuffer:
             seed (int): random seed
         """
         self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)
+        self.memory = []
         self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
+
+        self.rng = np.random.default_rng(seed)
+        self.min_priority = -1
+        self.total_priority = 0
+
+    def compute_partitions(self, alpha=.7):
+        partitions = []
+        N = len(self.memory)
+
+        # How much rank we're aiming for.
+        total_rank = 0
+        for i in range(N):
+            total_rank += np.power(1/(i + 1), alpha)
+
+        partition_prob = 1/self.batch_size
+        target_prob = partition_prob
+
+        cumulative_prob = 0
+        for i in range(N):
+            rank = i + 1
+            cumulative_prob += np.power(1/rank, alpha) / total_rank
+            if (cumulative_prob >= target_prob):
+                partitions.append(i)
+                if len(partitions) == self.batch_size - 1:
+                    break
+                target_prob = cumulative_prob + partition_prob
+
+        self.partitions = partitions
 
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
 
-    def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
+        # Heap requires a priority as the first tuple element. Use the memory index as a tie breaker.
+        e = (self.min_priority, len(self.memory), (state, action, reward, next_state, done))
+        self.memory.insert(0, e)
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+    def update_priorities(self, indices, priorities):
+        for index, priority in zip(indices, priorities):
+            # Heap is a min-heap, so invert the priority
+            priority = -priority
+            if priority < self.min_priority:
+                self.min_priority = priority
+            self.memory[index] = (priority, self.memory[index][1], self.memory[index][2])
+
+        # This probably shouldn't be run every time the priorities are updated
+        heapq.heapify(self.memory)
+
+    def sample(self, alpha=0):
+        """Randomly sample a batch of indices from memory."""
+        self.compute_partitions()
+        N = len(self.memory)
+        indices = []
+        start = 0
+
+        for end in self.partitions:
+            if start == end:
+                indices.append(end)
+            else:
+                indices.append(random.choice(range(start, end)))
+            start = end + 1
+
+        if start == N:
+            indices.append(N-1)
+        else:
+            indices.append(random.choice(range(start, N)))
+
+        if len(indices) < self.batch_size:
+            indices.extend(random.sample(range(N), self.batch_size - len(indices)))
+
+        return indices
+
+    def indices_to_experiences(self, indices):
+        experiences = [self.memory[i][2] for i in indices]
+
+        states = torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e[1] for e in experiences if e is not None])).long().to(device)
+        rewards = torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
 
         return (states, actions, rewards, next_states, dones)
 
